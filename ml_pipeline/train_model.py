d22ee0526ecf0c92a56ac592418b2d2429a65fbb
Dalzsfn <dalz20033@gmail.com>
2026-05-06 19:05:05 -0500

    entrenamiento del modelo, archivo gitignore añadido

--- ml_pipeline/train_model.py
+++ ml_pipeline/train_model.py
@@ -1,13 +1,27 @@
 import pandas as pd
 import joblib 
 from sklearn.ensemble import RandomForestClassifier
+from pathlib import Path
 
 def entrenar_modelo():
-    df = pd.read_csv('data/gym_churn_us.csv')
+    directorio_actual = Path(__file__).resolve().parent
+    ruta_datos = directorio_actual / 'data' / 'gym_churn_us.csv'
+    
+    # Cargar los datos
+    df = pd.read_csv(ruta_datos)
+    
     caida_frecuencia = df['Avg_class_frequency_total'] - df['Avg_class_frequency_current_month']
     df['caida_frecuencia'] = caida_frecuencia
     X = df.drop(columns=['Churn', 'Age', 'Avg_class_frequency_total'])
     y = df['Churn']
     
+    # Entrenamiento del modelo 
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X, y)
+    
+    # Guardar el modelo entrenado
+    ruta_modelo = directorio_actual / 'modelo_rf.pkl'
+    joblib.dump(model, ruta_modelo)
+    
+if __name__ == "__main__":   
+    entrenar_modelo()

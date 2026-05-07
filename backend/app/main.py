b3f53a2805e1f4a3b6d359b76a930094cf1bd80a
Dalzsfn <dalz20033@gmail.com>
2026-05-06 19:22:08 -0500

    endpoints iniciales

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}   
+
+@app.get("/clientes")
+def read_clientes():
+    return {"message": "Aquí se mostrarán los clientes"}
+
+@app.get("/predicciones")
+def read_predicciones():
+    return {"message": "Aquí se mostrarán las predicciones de churn"}

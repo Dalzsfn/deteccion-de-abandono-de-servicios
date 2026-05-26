d549b175f71833e90263fe48ff77bac406bdc94a
Dalzsfn <dalz20033@gmail.com>
2026-05-26 15:02:38 -0500

    cambio de modelo

--- backend/app/services/ml_service.py
+++ backend/app/services/ml_service.py
@@ -7,17 +7,21 @@ from backend.app.services import llm_service
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 MODEL_PATH = BASE_DIR / "ml_pipeline" / "modelo_rf.pkl"
 modelo = joblib.load(MODEL_PATH)
-explainer = shap.TreeExplainer(modelo)
+X_background = joblib.load(BASE_DIR / "ml_pipeline" / "features.pkl")
+scaler = joblib.load(BASE_DIR / "ml_pipeline" / "scaler.pkl")
+explainer = shap.LinearExplainer(modelo, X_background)
 
 def predecir_abandono(clientes):
     resultados = []
     for cliente in clientes:
         cliente_id = cliente.pop("cliente_id", None)
         df_cliente = pd.DataFrame([cliente])
-        prediccion = modelo.predict(df_cliente)[0]
+        df_cliente_escalado = scaler.transform(df_cliente)
+        prediccion = modelo.predict(df_cliente_escalado)[0]
         if prediccion == 1:
-            probabilidad = modelo.predict_proba(df_cliente)[0][1]
-            indice_motivo = int(explainer.shap_values(df_cliente)[0,:,1].argmax())
+            probabilidad = modelo.predict_proba(df_cliente_escalado)[0][1]
+            valores_shap = explainer.shap_values(df_cliente_escalado)
+            indice_motivo = int(valores_shap[0].argmax())
             motivo = df_cliente.columns[indice_motivo]
             sugerencia = llm_service.generar_sugerencia(motivo)
             resultados.append(
@@ -27,7 +31,6 @@ def predecir_abandono(clientes):
                     "probabilidad_abandono": float(probabilidad),
                     "motivo": motivo,
                     "sugerencia": sugerencia
-                    dfgdfsgdfg
                 }
             )
     return resultados
--- ml_pipeline/train_model.py
+++ ml_pipeline/train_model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import joblib 
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
 from pathlib import Path
 
 def entrenar_modelo():
@@ -15,13 +16,19 @@ def entrenar_modelo():
     X = df.drop(columns=['Churn', 'Avg_class_frequency_total', 'Phone', 'gender'])
     y = df['Churn']
     
+    # Estandarizar las características
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+    
     # Entrenamiento del modelo 
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model = LogisticRegression(class_weight='balanced', random_state=42)
     model.fit(X, y)
     
     # Guardar el modelo entrenado
     ruta_modelo = directorio_actual / 'modelo_rf.pkl'
     joblib.dump(model, ruta_modelo)
+    joblib.dump(X, directorio_actual / 'features.pkl')
+    joblib.dump(scaler, directorio_actual / 'scaler.pkl')
     
 if __name__ == "__main__":   
     entrenar_modelo()
